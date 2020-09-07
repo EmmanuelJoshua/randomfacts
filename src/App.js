3d497ebc122192bc73fe01841e735af6582c1ef5
Emmanuel Joshua <emmanueljoshua145@gmail.com>
2020-09-07 03:11:05 +0100

    App redesign

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Row, Container, Navbar, Col, Button } from 'react-bootstrap'
 import './App.css';
 
 class App extends Component {
@@ -8,9 +9,14 @@ class App extends Component {
     this.state = {
       randomFacts: {}
     };
+    this.getFact = this.getFact.bind(this);
   }
 
   componentDidMount() {
+   this.getFact();
+  }
+
+  getFact(){
     fetch("https://webknox-trivia-knowledge-facts-v1.p.rapidapi.com/trivia/random", { "method": "GET", "headers": { "x-rapidapi-host": "webknox-trivia-knowledge-facts-v1.p.rapidapi.com", "x-rapidapi-key": "6a17ef06f0mshcd455dbb738e1a5p1c6bf0jsn5206f1063964" } })
     .then(response => response.json())
     .then(data => this.setState({
@@ -23,7 +29,28 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {this.state.randomFacts.trivia}
+          <Navbar expand='lg' variant='dark' className='NavBar'>
+              <Navbar.Brand href='#home' className='NavBrand'>Random Facts</Navbar.Brand>
+          </Navbar>
+          <Container fluid>
+            <Row>
+                <Col className='RandomCard mt-4 ml-md-5'>
+                <div className="Header  text-white text-center pt-3 pb-3">
+                    <h1>Random Facts</h1>
+                    <hr/>
+                    <p className='pl-4 pr-4 pt-2'>
+                      {this.state.randomFacts.trivia}
+                      {/* 95% of people text things they could never say in person. */}
+                      </p>
+                      <Button variant='outline-light' className='mb-3 mt-1' onClick={this.getFact}>
+                          Get new fact
+                      </Button>
+                </div>
+                </Col>
+                <Col sm={4} md={6}></Col>
+            </Row>
+          </Container>
+         
         </header>
       </div>
     );
