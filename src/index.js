83888a1fb36da5b6238b09b7c9a7055049b4ac9f
Emmanuel Joshua <emmanueljoshua145@gmail.com>
2020-09-09 01:32:14 +0100

    Test

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './Pacifico-Regular.ttf'
-import './google.ttf'
+// import './Pacifico-Regular.ttf'
+// import './google.ttf'
 
-import 'bootstrap/dist/css/bootstrap.css'
+import './bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
